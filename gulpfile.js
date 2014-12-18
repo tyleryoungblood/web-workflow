97e0e7126093c8234ce94ba1d04317a240709f64
Tyler Youngblood <tyleryoungblood@yahoo.com>
2014-12-18 16:04:09 -0500

    add coffeeScript files and gulp-coffee plugin

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
+var coffeeSources = ['components/coffee/*.coffee'];
+
 gulp.task('coffee', function(){
-  gulp.src('components/coffee/tagline.coffee')
+  //gulp.src('components/coffee/tagline.coffee')
+  gulp.src(coffeeSources)
     .pipe(coffee({ bare:true })
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
