5c82c8ff7b961b96b044512d2583992bc2abea3d
Tyler Youngblood <tyleryoungblood@yahoo.com>
2014-12-18 16:12:48 -0500

    add ability to concatenate js files into one development/js/script.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@
 // all methods and properties form gulp (etc)
 // will be assigned to the gulp (etc) variables,
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    coffee  = require('gulp-coffee'), // process .coffee into .js
+    concat  = require('gulp-concat'); 
+
 
 var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources     = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js',
+]
 
 gulp.task('coffee', function(){
   //gulp.src('components/coffee/tagline.coffee')
@@ -15,3 +23,9 @@ gulp.task('coffee', function(){
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function(){
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
+})
