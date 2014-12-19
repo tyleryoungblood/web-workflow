4666e1fa6b35b8cb986e2450677443e2f08a24d8
Tyler Youngblood <tyleryoungblood@yahoo.com>
2014-12-19 11:26:57 -0500

    add compas process to gulpfile.js and added exercise files.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     coffee      = require('gulp-coffee'), // process .coffee into .js
     concat      = require('gulp-concat'),
+    compass     = require('gulp-compass'),
     browserify  = require('gulp-browserify'); 
 
 
@@ -15,7 +16,8 @@ var jsSources     = [
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js',
-]
+];
+var sassSources   = ['components/sass/style.scss']
 
 gulp.task('coffee', function(){
   //gulp.src('components/coffee/tagline.coffee')
@@ -30,4 +32,15 @@ gulp.task('js', function(){
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
-})
+});
+
+gulp.task('compass', function(){
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
+});
